2894744177ecf1b54f49471d107db792460701ca
Sahil <rk4852115@gmail.com>
2024-04-04 18:04:42 +0530

    04-04-2024

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:widgets_and_refactoring/home/component/5.2%20Task/main.dart';
+import 'package:widgets_and_refactoring/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
